ae8ce78bf1bdfaf8af892139feaf2ba99782357f
Ritvik <ritvyk@gmail.com>
2020-02-17 12:14:18 +0530

    Updated mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var finalScore=0;
 function startObstacles() {
     var colors = ["orange", "pink", "green", "violet", "brown", "white"];
     const rc = parseInt(Math.random() * 5);
@@ -8,11 +9,42 @@ function startObstacles() {
     obstacle.style.height = height;
     obstacle.className = "obstacle";
     obstacle.style.backgroundColor = colorName;
-    let container = document.getElementById("obstacle-container");
+    var container = document.getElementById("obstacle-container");
+    // console.log(container.children);
     container.appendChild(obstacle);
+    function getPlayerPosition()
+    {
+        var playerPosition=document.getElementById("player-model-img").getBoundingClientRect();
+        var cord={
+            "x":playerPosition.x,
+            "y":playerPosition.y
+        }
+        return cord;
+    }
     function moveLeft() {
         obstacle.style.right = move + "px";
         move += 10;
+        // if(move>window.screen.width)
+        // {
+        //     container.removeChild(obstacle);
+        // }
+        var obstaclePosition=obstacle.getBoundingClientRect();
+        var currentPlayerPostion=getPlayerPosition();
+        if(obstaclePosition.x<currentPlayerPostion.x+20 && obstaclePosition.x>currentPlayerPostion.x )
+        {
+            // again checking if the player is at the postion or not !!
+            // if(currentPlayerPostion.y>obstac)
+            console.log(currentPlayerPostion.y+30);
+            console.log(obstacle.clientHeight);
+            console.log("obstacle Y:"+obstaclePosition.y);
+            if(currentPlayerPostion.y+30 > obstaclePosition.y)
+            {
+                alert("Oops! Game Over Your Final Score : "+finalScore);
+                window.location.reload();
+                clearInterval(moveLeft);
+            }
+        }
+            
     }
     setInterval(moveLeft, 100);
 }
@@ -24,6 +56,7 @@ function startGame() {
     function increaseScore()
     {
         var score=parseInt(document.getElementById("score").innerText)+1;
+        finalScore=score;
         document.getElementById("score").innerText=score;
     }
     
